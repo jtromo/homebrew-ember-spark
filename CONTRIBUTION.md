f71b99632f0481513d3d8104a7d6a268ea48f501
James Romo <james.romo@workiva.com>
2018-01-03 10:05:23 -0700

    Updated qdart.rb to 1.1.0 and added contribution file.

---
+++ CONTRIBUTION.md
@@ -0,0 +1,6 @@
+# Contribution
+
+1. Submit a pull request to the formula's project (https://github.com/jtromo/qdart)
+2. Request to cut a release (using semver versioning)
+3. Edit the formula (qdart.rb) to point to the new release with an updated SHA256
+4. Submit a pull request with the updated formula
--- Formula/qdart.rb
+++ Formula/qdart.rb
@@ -1,8 +1,8 @@
 class Qdart < Formula
   desc "Quick Dart provides commands to make life using dart and pub easier"
   homepage "https://github.com/jtromo/qdart"
-  url "https://github.com/jtromo/qdart/archive/1.0.0.tar.gz"
-  sha256 "bb66655d8931d3592431d2ef4282554a18328c9c7c4af7508dffa22a50aec517"
+  url "https://github.com/jtromo/qdart/archive/1.1.0.tar.gz"
+  sha256 "6416affdc4d126171b9110429282522161e843d8a082b5e3b4645900230104c0"
 
   bottle :unneeded
 
