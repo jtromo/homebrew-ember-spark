a4200650037cc74e85fca0068b16b6ec8b952bfd
James Romo <jamestromo@gmail.com>
2018-02-26 00:10:24 -0700

    Add contributions to readme. qdart 1.3.0

--- Formula/qdart.rb
+++ Formula/qdart.rb
@@ -1,8 +1,8 @@
 class Qdart < Formula
   desc "Quick Dart provides commands to make life using dart and pub easier"
   homepage "https://github.com/jtromo/qdart"
-  url "https://github.com/jtromo/qdart/archive/1.2.0.tar.gz"
-  sha256 "b7a3fb2be789e62fb4cb864d9aa4e54b7018f31387fc042a542f39f6cdbf2eea"
+  url "https://github.com/jtromo/qdart/archive/1.3.0.tar.gz"
+  sha256 "1a36e620a6148aceeed4ecf90285a6928c7b134f105a52ba3e13e29236f9bf05"
 
   bottle :unneeded
 
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Homebrew Ember Spark
 Ember Spark Software formulae for the Homebrew package manager.
 
+## Contributions
+[Contribution guidelines for this project](.github/CONTRIBUTING.md)
+
 ## Installation
 ```brew install jtromo/homebrew-ember-spark/<formula>```
 
